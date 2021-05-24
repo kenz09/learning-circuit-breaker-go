4408f9e844ba5d684e3ed579d9817b2db6834a5e
kenz09 <kenzieleonaro@gmail.com>
2021-05-24 09:00:53 +0700

    simple example

--- example-circuit-breaker-go/circuitBreaker.go
+++ example-circuit-breaker-go/circuitBreaker.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"time"
 )
 
 type CircuitBreaker interface {
@@ -16,27 +17,36 @@ type CircuitBreaker interface {
 }
 
 type CircuitBreakerImpl struct {
-	lastError                      error
-	state                          string
-	failureCounter                 int
-	successCounter                 int
-	failureTreshold                int
-	successTreshold                int
-	allowedHalfOpenRequestTreshold int
+	openStateTimeout time.Time
+	lastError        error
+	state            string
+	failureCounter   int
+	failureTreshold  int
 }
 
 func (this *CircuitBreakerImpl) Execute(f func() (interface{}, error)) (interface{}, error) {
-	if !this.IsClosed() {
+	if !this.IsClosed() && this.State() != "half-open" {
 		return nil, errors.New("Circuit breaker tripped!")
 	}
 	r, e := f()
 	if e != nil {
-		this.failureCounter++
-		if this.failureCounter > this.failureTreshold {
+		if this.State() == "half-open" {
 			this.Trip(e)
+		} else {
+			this.failureCounter = this.failureCounter + 1
+			if this.failureCounter > this.failureTreshold {
+				this.Trip(e)
+			}
+		}
+	} else {
+		this.failureCounter = 0
+		if this.State() == "half-open" {
+			this.Reset()
 		}
 	}
-
+	if e != nil {
+		fmt.Println(e.Error())
+	}
 	return r, e
 }
 
@@ -47,19 +57,26 @@ func (this *CircuitBreakerImpl) State() string {
 func (this *CircuitBreakerImpl) Reset() {
 	this.state = "closed"
 	this.failureCounter = 0
+	fmt.Println("Circuit Reset")
 }
 
 func (this *CircuitBreakerImpl) HalfOpen() {
 	this.state = "half-open"
+	fmt.Println("Circuit state changed to half-open")
 }
 
 func (this *CircuitBreakerImpl) Trip(err error) {
-	fmt.Println("circuit tripped")
+	fmt.Println("circuit tripped to open")
 	fmt.Println(err.Error())
 	this.lastError = err
 	this.state = "open"
 	this.failureCounter = 0
-	this.successCounter = 0
+	this.openStateTimeout = time.Now().Add(30 * time.Second)
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		this.HalfOpen()
+	}()
 }
 
 func (this *CircuitBreakerImpl) IsClosed() bool {
--- example-circuit-breaker-go/main.go
+++ example-circuit-breaker-go/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -8,6 +13,62 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	cb1 := &CircuitBreakerImpl{
+		failureTreshold: 3,
+		state:           "closed",
+	}
+
+	cb2 := &CircuitBreakerImpl{
+		failureTreshold: 3,
+		state:           "closed",
+	}
+
+	r.GET("/services/service1", func(c *gin.Context) {
+		res, err := cb1.Execute(func() (interface{}, error) {
+			resp, e := http.Get("localhost:8001/ping")
+			if e != nil {
+				return nil, e
+			}
+
+			return resp, nil
+		})
+		if err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+
+		resResp := res.(*http.Response)
+		resBody, err := ioutil.ReadAll(resResp.Body)
+		if err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+
+		c.JSON(200, string(resBody))
+	})
+	r.GET("/services/service2", func(c *gin.Context) {
+		res, err := cb2.Execute(func() (interface{}, error) {
+			resp, e := http.Get("http://localhost:8002/ping")
+			if e != nil {
+				return nil, e
+			}
+
+			return resp, nil
+		})
+		if err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+
+		resResp := res.(*http.Response)
+		resBody, err := ioutil.ReadAll(resResp.Body)
+		if err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+
+		c.JSON(200, string(resBody))
 	})
 	r.Run() //listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
